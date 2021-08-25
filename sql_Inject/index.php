e09f50a3dc95c98d761a90aec55ad205a0ef0076
Alessandro Da Silva Dias <alessandrobravim52@gmail.com>
2021-08-25 15:14:19 -0300

    explicao sobre a funcao addslashes

--- sql_Inject/index.php
+++ sql_Inject/index.php
@@ -3,4 +3,7 @@
                               //   '; DROP TABLE posts; -> so com esse sql posso deleta todos posts
             // O addslashes ele coloca / barras quando poim algumas ' apas para nao quebra
     $sql = "SELECT * from posts WHERE autor = ''";
+
+    // Nunca podemos cria uma variavel para faz um post igual eu fisso sem faz
+    // a funcao addslashes
 ?>
