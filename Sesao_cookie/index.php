6fef6cd0a905d5a9f1d4914cfba85f4ba47ce700
Alessandro Da Silva Dias <alessandrobravim52@gmail.com>
2021-08-25 17:07:11 -0300

    Aula de cookie e Sessão

---
+++ Sesao_cookie/index.php
@@ -0,0 +1,33 @@
+<?php
+    session_start(); // ela tem que ser a primeira linha do codigo
+
+    // Gera uma sessao
+    $_SESSION["teste"] = "Alessandro";
+    echo "Minha sessão e de: ".$_SESSION["teste"];
+    echo "<br>";
+
+    // gera um cookie
+    setcookie("meuteste", $_SESSION["teste"],  time()+3600);
+    echo "Meu cookie e de: ".$_COOKIE["meuteste"]; // printei o cookie
+
+
+
+
+
+
+
+
+
+
+
+    // setcookie("meuteste", "fulano de tal", time()+3600);
+
+    // echo "cookie setado com sucesso";
+
+    // session_start();
+    // $_SESSION["teste"] = array (
+    //     "alessandro" // podemos faz um array dentro de uma session
+    // );
+    // $_SESSION['teste'] = "alessandro da silva dias"; // O navegado vai salva estes dados
+    // echo "Meu nome e: " .$_SESSION["teste"];
+?>
