1a6417aaedb3c964a9b38a2a0837b3a8d7884382
Alessandro Da Silva Dias <alessandrobravim52@gmail.com>
2021-08-27 14:19:38 -0300

    voltando o minha edicao trazada do insert que eu apaguei

--- intermediario/pdo_insert_delet.etc/select/pdo_Select.php
+++ intermediario/pdo_insert_delet.etc/select/pdo_Select.php
@@ -7,7 +7,21 @@ $dbpass = "";
 try {
     $pdo = new PDO($dsn, $dbuser, $dbpass);
 
-    
+    $sql = "SELECT * FROM usuarios WHERE email = 'alessandrobravim52@gmail.com'";
+    $sql = $pdo->query($sql);
+
+    /* row segnifica linha 
+    * Count segnifica contar
+    */
+
+    if ($sql->rowCount() > 0) {
+
+        foreach ($sql->fetchAll() as $usuario) {
+            echo "nome: ".$usuario["nome"]."| email: ".$usuario["email"]."<br>";
+        }
+    } else {
+        echo "Não há usuários cadastrados!";
+    }
 
 } catch(PDOException $e) {
     echo "falhou: ".$e->getMessage();
